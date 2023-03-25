88a4c504d08fe6be897c42658c575b395f84bdda
Andoni AT <andoni.alonso-tort@etu.univ-lehavre.fr>
2023-03-25 11:42:17 +0100

    Zone pour dessiner pret et fonctionnelle et chat a côté de la zone dessin

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ let wsname = cookies.find(function(c) {
   if (c.match(/wsname/) !== null) return true;
   return false;
 });
+
 if (isDef(wsname)) {
   wsname = wsname.split('=')[1];
 } else {
@@ -34,6 +35,7 @@ ws.onopen = (event) => {
 const messages = document.querySelector('#messages');
 let line;
 ws.onmessage = (event) => {
+  console.log("on message");
   line = document.createElement('li');
   line.textContent = event.data;
   messages.appendChild(line);
@@ -49,8 +51,94 @@ function sendMessage(event) {
     sendInput.value = '';
   }
 }
-const sendForm = document.querySelector('form');
-const sendInput = document.querySelector('form input');
+const sendForm = document.getElementById("formMsg");
+const sendInput = document.querySelector('#formMsg input');
 sendForm.addEventListener('submit', sendMessage, true);
 sendForm.addEventListener('blur', sendMessage, true);
 
+window.addEventListener("load", () => {
+
+  const divColor = document.getElementById("divColor");
+  
+  // Click sur le div color active l'input color
+  divColor.addEventListener("click", function() {
+      const color = document.getElementById("color-picker");
+      color.click(); // action du click sur l'input color
+      let $this = this;
+      
+      // Quand l'input change la valeur background du div change aussi
+      color.addEventListener("change", function() {
+        $this.style.backgroundColor = this.value;
+      });
+    });
+
+  // Recuperer le canvas et le context 2d
+  const canv = document.getElementById("draw");
+  const context = canv.getContext("2d");
+
+  // Initialiser positions
+  let initialX;
+  let initialY;
+
+  // Fonction pour dessiner
+  const draw = (cursorX, cursorY) => {
+    const color = document.getElementById("color-picker");
+    const stroke = document.getElementById("stroke");
+    
+    context.beginPath();
+    context.moveTo(initialX, initialY);
+    context.lineWidth = stroke.value;
+    context.strokeStyle = color.value;
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.lineTo(cursorX, cursorY);
+    context.stroke();
+
+    initialX = cursorX;
+    initialY = cursorY;
+  } ;
+
+  // Quand on fait un click dans la souris
+  const mouseDown = (evt) => {
+    let val = valueMouse(evt.clientX, evt.clientY);
+    initialX = val.x;
+    initialY = val.y;
+    draw(initialX, initialY); // Dessiner
+
+    // Mouvement de la souris lorsque l'on presse le boutton
+    canv.addEventListener("mousemove", mouseMoving);
+  };
+
+  //Positions exactes
+  const valueMouse = (x, y) => {
+    // Obtenir positions par rapport a la fenetre
+    let rect = canv.getBoundingClientRect();
+    let objX = rect.left;
+    let objY = rect.top;
+
+    // Calculer la position de la souris par rapport à l'objet
+    // moin la position de l'objet par rapport à la fenêtre
+    let relX = x - objX;
+    let relY = y - objY;
+  
+    return {
+      x: relX,
+      y: relY
+    };
+  };
+
+  // S'arreter quand on lache le boutton de la souris
+  const mouseUp = () => {
+    canv.removeEventListener("mousemove", mouseMoving);
+  }
+
+  // Mouvement de la souris 
+  const mouseMoving = (evt) => {
+    // Calculer x et y
+    let val = valueMouse(evt.clientX, evt.clientY);
+    draw(val.x, val.y); // Dessiner
+  }
+
+  canv.addEventListener("mousedown", mouseDown);
+  canv.addEventListener("mouseup", mouseUp);
+});
